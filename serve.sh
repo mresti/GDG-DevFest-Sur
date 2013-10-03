42e479a88577c112a65b9c25eefd167735d8e57c
Esteban Dorado Roldan <fadoro@gmail.com>
2013-10-03 17:00:44 +0200

    updated buttons

---
+++ serve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Starts a basic web server on the port specified.
+# 
+# ./serve.sh 3000 -> http://localhost:3000
+#
+# Copyright 2012 Eric Bidelman <ebidel@gmail.com>
+
+python -m SimpleHTTPServer 8000
