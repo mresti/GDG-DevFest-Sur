7748c4dc73d80a0bb6a75f38192614ebc42b2c4e
s1yamuza <asyamuza@gmail.com>
2013-10-24 18:32:06 +0200

    added gold sponsor logos and location map

--- _patrocinadores.php
+++ _patrocinadores.php
@@ -17,10 +17,15 @@
               <h3>DIAMANTE</h3>
 						</div>
 						<p><a href="https://www.google.com/" target="_blank"><img src="img/Google_Logo.png" alt="Google logo" border="0"></a></p>
-            <div class="head well">
+            <div class="head well ">
               <h3>ORO</h3>
 						</div>
-						<p>Sé el primer patrocinador oro del DevFest Sur</p>
+						<p class="aligncenter">
+						<a href="https://www.uco.es/organizacion/consejosocial" target="_blank">
+							<img width="220" height="140"  src="img/consejo_social_uco_logo_1.png" alt="Consejo Social UCO logo" border="0">
+							<img class="separator" width="230" height="110" src="img/consejo_social_uco_logo_2.png" alt="Consejo Social UCO logo" border="0">
+						</a>
+						</p>
             <div class="head well">
               <h3>PLATA</h3>
 						</div>
