574b3395def4e31f44069153ff39b3f8a8f03e2a
hidabe <hidabe@hidabe-W130EW.(none)>
2013-10-24 21:29:57 +0200

    Ponentes pasados a JSON

--- _ponentes.php
+++ _ponentes.php
@@ -12,99 +12,34 @@
          </div>
 				</div>
 
-				<div class=row">
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-									<div class="rrss">
-										<a href="http://www.linkedin.com/in/julianmb"><img src="img/rrss/LinkedIn.png"></a>										
-										<a href="https://twitter.com/julianmb"><img src="img/rrss/Twitter.png"></a>
-										<a href="https://plus.google.com/102043168453367495516"><img src="img/rrss/Google.png"></a>
-									</div>
-                  <h3>Julián Beltrán <span class="ameta">CEO at Droiders</span></h3>
-                  <p> <img src="img/speakers/julianbeltran.jpg" alt="" class="img-circle"/>Emprendedor del sector de las tecnologías de la información y comunicaciones. Fundador de Beltia y deDroiders, empresa especializada en aplicaciones para Android.</p>
-									<div style="clear:both"></div>
-               </div>
-            </div>
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-									<div class="rrss">
-										<a href="http://www.linkedin.com/in/alfredoartiles"><img src="img/rrss/LinkedIn.png"></a>										
-										<a href="https://twitter.com/aartiles24"><img src="img/rrss/Twitter.png"></a>
-										<a href="https://plus.google.com/108635286356285102317"><img src="img/rrss/Google.png"></a>
-									</div>
-                  <h3>Alfredo Artiles <span class="ameta">SocialBro Co-founder and CTO</span>
-									</h3>
-                  <p> <img src="img/speakers/alfredoartiles.jpg" alt="" class="img-circle"/>Cubano y cordobés de adopción, es licenciado en Ciencias de la Computación por la Universidad Central de Las Villas y lleva 12 años desarrollando proyectos en Internet.</p>
-									<p>Co-fundador y director de tecnología de equipo24 S.L., empresa especializada en el desarrollo de soluciones socialmedia. Ha liderado el desarrollo de la plataforma de presentadores virtualese24Presenter y de la mayor red de aplicaciones de Twitter en España: e24Apps Network, donde destacan aplicaciones comoFollowFriday.com, Resonancers.com, FilterTweeps.com, FriendLynx, LocaFollow.com, entre otras. Actualmente lidera el desarrollo de SocialBro, una herramienta de análisis y gestión de contactos en Twitter con una gran tendencia a CRM social.</p>
-									<div style="clear:both"></div>
-               </div>
-            </div>
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-									<div class="rrss">
-										<a href="http://www.linkedin.com/in/asierarranz"><img src="img/rrss/LinkedIn.png"></a>										
-										<a href="https://twitter.com/asierarranz"><img src="img/rrss/Twitter.png"></a>
-										<a href="https://plus.google.com/108749679517324971736"><img src="img/rrss/Google.png"></a>
-									</div>
-                  <h3>Asier Arranz <span class="ameta">Using technology to create emotions</span></h3>
-                  <p> <img src="img/speakers/asierarranz.jpg" alt="" class="img-circle" />I have a very strong knowledge in all technology áreas, combined with business skills adquired though my 2 startups Nebutek & Atlanpix (1 alive, 1 failed).</p>
-									<p>I raised an investment from Wayra Spain, and made technical due diligences to Job and Talent, Blink and Mimub. Months later JobandTalent raised $3.3M and Blink was adquired by Groupon.</p>
-									<p>I have been Global Developer Advisor for Campus Party Europe; Leader and organizer of NASA Space Apps Challenge in Spain, TEDx Speaker (Talk: "Virtual reality, real perception: Tricking our brain"), teacher at IE Business School and Repsol. Mentor at Startup Weekend, NetDay and Acelera X2 Colombia. I have made tech demostrations at Microsoft and Telefónica. And I usually collaborate teaching technology activities for kids.</p>
-									<div style="clear:both"></div>
-               </div>
-            </div>
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-									<div class="rrss">
-										<a href="http://www.linkedin.com/in/alfredoromeo"><img src="img/rrss/LinkedIn.png"></a>										
-										<a href="https://twitter.com/aromeo"><img src="img/rrss/Twitter.png"></a>
-										<a href="https://plus.google.com/108696100362557238756"><img src="img/rrss/Google.png"></a>
-									</div>
-                  <h3>Alfredo Romeo <span class="ameta">CEO at Intelify, nowadays, connecting objects and people to the Internet</span></h3>
-                  <p> <img src="img/speakers/alfredoromeo.jpg" alt="" class="img-circle" />Alfredo Romeo (@aromeo) conceptualiza, diseña, testa y emprende en diferentes proyectos que tienen que ver con cómo conectar el entorno físico e Internet desde el año 2000.</p>
-									<p>Desde el 2010, trabaja en la conceptualización, diseño y desarrollo de Intelify, una plataforma para conectar el mundo físico a Internet de manera fácil, simple, sencilla, y rentable. Intelify es independiente de la tecnologia. Fruto de esta plataforma han nacido diferentes productos como Do it social, City Analytics e Intelify Tags.</p>
-									<div style="clear:both"></div>
-               </div>
-            </div>
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-									<div class="rrss">
-										<a href="http://es.linkedin.com/in/neonigmacdb‎"><img src="img/rrss/LinkedIn.png"></a>										
-										<a href="https://twitter.com/neonigmacdb"><img src="img/rrss/Twitter.png"></a>
-									</div>
-                  <h3>José Ignacio Álvarez <span class="ameta">Desarrollador Android en WUL4</span></h3>
-                  <p> <img src="https://si0.twimg.com/profile_images/1554458558/4twitter_bigger.png" alt="" class="img-circle"/>Ingeniero en Informática que desde hace varios años desarrolla profesionalmente su pasión por el software libre combinando tecnologías Web, de escritorio y móviles. Entre las tecnologías que domina se encuentran Python, PHP, MySQL, REST, XMLRPC y Android. Muestra especial habilidad en la integración entre aplicaciones, destacando el desarrollo de inicio de sesión unificado para distribuciones GNU/Linux como Guadalinex Telecentros y Guadalinex Guadalinfo, así como optimización de aplicaciones Python y desarrollo de aplicaciones de variado tipo para Android.</p>
-									<div style="clear:both"></div>
-               </div>
-            </div>
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-                  <h3>Javier Gutierrez <span class="ameta">- Profesor en Universidad de Sevilla.</span></h3>
-                  <p> <img src="img/speakers/javier.png" alt="" class="img-circle"/>Formador especializado en ingeniería del software, metodologías en administraciones públicas y prueba del software.</p><p>Especialidades: ingeniería del software, desarrollo de sistemas de información en administraciones públicas, prueba del software.</p></div>
-									<div style="clear:both"></div>
-               </div>
-            </div>
-            <div class="span12">
-               <div class="about">
-                  <!-- Groom details with photo -->
-									<div class="rrss">
-										<a href="http://www.linkedin.com/in/hidabe"><img src="img/rrss/LinkedIn.png"></a>										
-										<a href="https://twitter.com/hidabe"><img src="img/rrss/Twitter.png"></a>
-										<a href="https://plus.google.com/109623390521488791490‎"><img src="img/rrss/Google.png"></a>
-									</div>
-                  <h3>Fernando Hidalgo <span class="ameta">- Analista/Programador, fundador de Sopinet.</span></h3>
-                  <p> <img src="https://si0.twimg.com/profile_images/2292970656/k2ij8tnox9mkiiigltdt_bigger.png" alt="" class="img-circle"/>Desarrollador web y tecnologías smartphones, analista/programador, mantenimiento de sistemas y servidores linux.</p>
-									<p>Fundador de la empresa Sopinet, donde desarrollamos soluciones web/app sencillas para problemas no triviales.</p>
-									<p>Me gusta ser ágil, eficaz y profesional en mi trabajo. Hago uso de frameworks, proyectos software libre y amo los gestores de dependencias y la modularización como vía para agilizar proyectos al máximo.</p>
-									<div style="clear:both"></div>
-               </div>
-            </div>            
-				</div>
+				
+<?php
+	$string = file_get_contents("_ponentes.json");
+	$json_a=json_decode($string,true);
+	foreach($json_a['ponentes'] as $ponente) { ?>
+		<div class=row">
+      <div class="span12">
+         <div class="about">
+            <!-- Groom details with photo -->
+						<div class="rrss">
+							<?php if ($ponente['linkedin'] != "") { ?>
+								<a href="<?php echo $ponente['linkedin'];?>"><img src="img/rrss/LinkedIn.png"></a>
+							<?php } ?>
+							<?php if ($ponente['twitter'] != "") { ?>			
+								<a href="<?php echo $ponente['twitter'];?>"><img src="img/rrss/Twitter.png"></a>
+							<?php } ?>
+							<?php if ($ponente['google'] != "") { ?>
+								<a href="<?php echo $ponente['google'];?>"><img src="img/rrss/Google.png"></a>
+							<?php } ?>
+						</div>
+            <h3><?php echo $ponente['nombre'];?> <span class="ameta"><?php echo $ponente['cargo'];?></span></h3>
+            <p> <img src="<?php echo $ponente['imagen'];?>" alt="" class="img-circle"/><?php echo $ponente['descripcion'];?></p>
+						<div style="clear:both"></div>
+         </div>
+      </div>
+		</div>
+	<?php }
+?>           
+		
 		</div>
 	</div>
