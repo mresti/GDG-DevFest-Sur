91318eedefc5119cbd79a0399f216649b9e82e3a
s1yamuza <asyamuza@gmail.com>
2013-10-25 17:23:07 +0200

    collaboratos without name

--- _colaboradores.php
+++ _colaboradores.php
@@ -13,31 +13,18 @@
         </div>
 
         <div class="col-md-3">
-            <div class="head well">
-              <h4><a href="http://www.cosfera.es">coSfera</a></h4>
-            </div>
             <p><a href="https://www.cosfera.es/" target="_blank"><img width="160" height="150" src="img/coSfera_vertical.png" title="coSfera" alt="coSfera" border="0"></a></p>
         </div>
 
         <div class="col-md-3">
-            <div class="head well">
-              <h4><a href="http://www.deidos.com">Deidos</a></h4>
-            
-            </div>
             <p><a href="http://www.deidos.com/" target="_blank"><img width="160" height="150" src="img/logo_deidos_cuadrado.png" title="Deidos Studio" alt="Deidos Studio" border="0"></a></p>
         </div>
 
         <div class="col-md-3">
-            <div class="head well">
-              <h4><a href="http://sopinet.com">sopinet</a></h4>
-            </div>
             <p><a href="http://sopinet.com/" target="_blank"><img width="140" height="140" src="img/sopinet_cuadrado_alta.png" title="Sopinet" alt="Sopinet" border="0"></a></p>
         </div>
 
         <div class="col-md-3">
-            <div class="head well">
-              <h4><a href="http://zendeweb.com">zendeweb</a></h4>
-            </div>
             <p><a href="http://zendeweb.com/" target="_blank"><img width="180" height="135" src="img/zendeweb_cuadrado.png" title="Zendeweb" alt="Zendeweb" border="0"></a></p>
         </div>
     </div>
