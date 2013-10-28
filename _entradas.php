6c4784dba982f9b765d8034d991f83f617d28609
hidabe <hidabe@hidabe-W130EW.(none)>
2013-10-28 23:33:55 +0100

    Fix app link

--- _entradas.php
+++ _entradas.php
@@ -17,7 +17,10 @@
                     <p>Vente al I GDG DevFest Sur que se realizará en Córdoba, el próximo 31 de Octubre. Reserva tu entrada GRATUITA ahora.</p>
                     </div>
                     <!-- Button. Note down the class "button" -->
-                    <div class="row"><a class="action-button" href="http://intelify.net/eventos/gdgdevfestsur2013">¡Consigue tu entrada!</a></div>
+                    <div class="row">
+											<a class="action-button" href="http://intelify.net/eventos/gdgdevfestsur2013">¡Consigue tu entrada!</a>
+											<a href="https://play.google.com/store/apps/details?id=com.gdgdevfest.android.apps.devfestsur&hl=es" title="Descarga la app para 	Android"><img src="img/google-play.png" alt="Descarga la app en Play Store"/></a>
+										</div>
                     </div>
                 </div>
             </div>
