b1b68a7c41d15c946dd3f3323ec72f34b4047db0
hidabe <hidabe@hidabe-W130EW.(none)>
2013-11-04 13:28:44 +0100

    Archivado

--- _entradas.php
+++ _entradas.php
@@ -6,19 +6,19 @@
                 <div class="col-md-12">
                     <div class="hero">
                     <!-- Getting married -->
-                    <h1>¿Te vienes al GDG DevFest Sur?</h1>
+                    <h1>¡Hasta el próximo GDG DevFest Sur!</h1>
                     <div class="well">
                     <!-- date -->
-                    <h3>Córdoba, 31 de Octubre, 2013</h3>
+                    <h3>Será en 2014...</h3>
                     <!-- Use the below link to create line with heartin -->
                     <!-- Name -->
                     <img src="img/gdgdevfest.png" height="130" alt="" />
                     <!-- Small para -->
-                    <p>Vente al I GDG DevFest Sur que se realizará en Córdoba, el próximo 31 de Octubre. Reserva tu entrada GRATUITA ahora.</p>
+                    <p>Todo un éxito este evento. Gracias a coSfera, Deidos, Zendeweb, Sopinet, Esteban, Antonio Fernández, Miguel Calero... ¡y a todos vosotros!</p>
                     </div>
                     <!-- Button. Note down the class "button" -->
                     <div class="row">
-											<a class="action-button" href="http://intelify.net/eventos/gdgdevfestsur2013">¡Consigue tu entrada!</a>
+<!--											<a class="action-button" href="http://intelify.net/eventos/gdgdevfestsur2013">¡Consigue tu entrada!</a>-->
 											<a href="https://play.google.com/store/apps/details?id=com.gdgdevfest.android.apps.devfestsur&hl=es" title="Descarga la app para 	Android"><img src="img/google-play.png" alt="Descarga la app en Play Store"/></a>
 										</div>
                     </div>
